dcc48478e3a6b511ab5fa6a6f5c53e8dcc157808
tkaczenko <include.free@gmail.com>
2017-12-09 17:21:25 +0200

    updated labs;

--- src/main/scala/io/github/tkaczenko/system_modulating/UniformGenerator.scala
+++ src/main/scala/io/github/tkaczenko/system_modulating/UniformGenerator.scala
@@ -1,13 +1,26 @@
+import java.io.{File, PrintWriter}
 
+import scala.collection.mutable.ListBuffer
 
 object UniformGenerator extends App {
   override def main(args: Array[String]): Unit = {
     val numbersCount = 10000
     val generatorSeed = 27000
-    var generatedValues = new List[Double]()
+    var generatedValues = ListBuffer[Double]()
     val uniformDistribution: UniformDistribution = UniformDistribution(seed = generatorSeed)
     for (i <- 0 until numbersCount) {
       generatedValues += uniformDistribution.next()
     }
+    val writer = new PrintWriter(new File("generated.txt"))
+    generatedValues.foreach(generatedValue => {
+      writer.write(generatedValue + "\n")
+    })
+
+    val minValue = generatedValues.min
+    val maxValue = generatedValues.max
+    println("Min value = " + minValue)
+    println("Max value = " + maxValue)
+
+    var intervalsCount =
   }
 }
